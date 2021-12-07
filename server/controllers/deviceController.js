0bcefec1674ba5390ed826033575095bc493bba9
SirJoraH21 <mr.vip.yurra90@gmail.com>
2021-12-07 16:23:44 +0200

    added controllers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,15 @@
+class DeviceContoller {
+  async create(req, res) {
+
+  }
+
+  async getAll(req, res) {
+
+  }
+
+  async getOne(req, res) {
+
+  }
+}
+
+module.exports = new DeviceContoller();
---
+++ server/controllers/typeController.js
@@ -0,0 +1,11 @@
+class TypeContoller {
+  async create(req, res) {
+
+  }
+
+  async getAll(req, res) {
+
+  }
+}
+
+module.exports = new TypeContoller();
