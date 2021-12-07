5db8075a4f62faffb15bdda770f6cd459a35e2af
SirJoraH21 <mr.vip.yurra90@gmail.com>
2021-12-07 20:19:26 +0200

    added device controller

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const sequelize = require('./db');
-
 const router = require('./routes/index');
 const models = require('./models/models');
 const errorHandler = require('./middlware/errorHandlingMidlware');
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 // Error handling, last middlware
