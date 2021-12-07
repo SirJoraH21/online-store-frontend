8b50a2fe82c009c7024b300548eb5cab29765f49
SirJoraH21 <mr.vip.yurra90@gmail.com>
2021-12-07 23:51:27 +0200

    Added delete type route

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -18,6 +18,18 @@ class TypeContoller {
 
     return res.json(types);
   }
+
+  async delete(req, res, next) {
+    const { id } = req.params;
+
+    if (!id) res.json({ message: 'id query parameter is missed' });
+
+    const type = await Type.findOne({ where: { id } });
+    if (!type) next(ApiError.badRequest(`Type with id:${id} didn't exist`));
+
+    await type.destroy();
+    return res.status(200).json({ message: `Type ${type.name} was deleted` });
+  }
 }
 
 module.exports = new TypeContoller();
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -6,5 +6,6 @@ const router = new Router();
 
 router.post('/', checkRole('admin'), typeController.create);
 router.get('/', typeController.getAll);
+router.delete('/:id', checkRole('admin'), typeController.delete);
 
 module.exports = router;
